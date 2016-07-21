d4180e70ff894f48a1dd18518a3ac34888b54c82
Zbyszek Swirski <Zbyszek Swirski@ZBYSZEK-PC>
2016-07-21 15:55:52 +0100

    First release

---
+++ KML2Json/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KML2Json
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            string line;
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                // Read the file and display it line by line.
+                var fileName = dlg.FileName;
+                var file = new StreamReader(fileName);
+                var outfileName = Path.GetFileNameWithoutExtension(fileName) + ".json";
+                var outfile = new StreamWriter(outfileName);
+                var state = -1;
+                var name = "";
+                var firstelement = true;
+                while ((line = file.ReadLine()) != null)
+                {
+                    switch (state)
+                    {
+                        case -1:
+                            if (line.StartsWith("<Placemark")) state = 0;
+                            break;
+                        case 0:
+                            if (line.StartsWith("</Placemark"))
+                            {
+                                outfile.WriteLine(EndBlock(name));
+                            } else
+                            if (line.StartsWith("<name"))
+                            {
+                                name = Regex.Match(line, ">(.*)<").Groups[1].Value;
+                                outfile.Write(StartBlock(name));
+                            } else
+                            if (line.StartsWith("<coordi"))
+                            {
+                                state = 1;
+                                outfile.Write("[");
+                                firstelement = true;
+                            }
+                            break;
+                        case 1:
+                        {
+                            if (line.StartsWith("</coord")) { state = 0; outfile.Write("]"); continue;}
+                            if (!firstelement) outfile.Write(",");
+                            else firstelement = false;
+                                    outfile.Write("["+line+"]");
+                                
+                            break;
+                        }
+                    }
+                }
+                outfile.Flush();
+                outfile.Close();
+                file.Close();
+            }
+        }
+
+        private static string StartBlock(string name)
+        {
+            return "{ \"type\": \"Feature\",\"properties\": { \"name\": \"" + name +
+                   "\" },\"geometry\": {\"type\": \"MultiPolygon\",\"coordinates\": [[";
+        }
+
+        private static string EndBlock(string name)
+        {
+            return "]]},\"id\": \"POL-" + name + "\"},";
+        }
+    }
+}
---
+++ Scripts/index.js
@@ -0,0 +1,128 @@
+﻿
+//simple XHR request in pure JavaScript
+function load(url, callback) {
+    var xhr;
+    var def = $.Deferred();
+    if (typeof XMLHttpRequest !== 'undefined') xhr = new XMLHttpRequest();
+    else {
+        var versions = ["MSXML2.XmlHttp.5.0",
+            "MSXML2.XmlHttp.4.0",
+            "MSXML2.XmlHttp.3.0",
+            "MSXML2.XmlHttp.2.0",
+            "Microsoft.XmlHttp"]
+
+        for (var i = 0, len = versions.length; i < len; i++) {
+            try {
+                xhr = new ActiveXObject(versions[i]);
+                break;
+            }
+            catch (e) { }
+        } // end for
+    }
+
+    xhr.onreadystatechange = ensureReadiness;
+
+    function ensureReadiness() {
+        if (xhr.readyState < 4) {return;}
+
+        if (xhr.status !== 200) {return;}
+
+        // all is well
+        if (xhr.readyState === 4 && callback) { callback && callback(xhr); def.resolve(xhr);  }
+    }
+
+    xhr.open('GET', url, true);
+    xhr.send('');
+    return def.promise();
+}
+
+
+//setup the map
+var map = new L.Map("map", {
+    center: [50,22],
+    zoom: 3
+})
+    .addLayer(new L.TileLayer("http://{s}.tile.cloudmade.com/1a1b06b230af4efdbb989ea99e9841af/998/256/{z}/{x}/{y}.png"));
+var contacts, colors, regions, countries;
+var getcolors = load('/data/country_colors.json', function (xhr) { colors = JSON.parse(xhr.responseText); });
+var getcontacts = load('/data/contacts.json', function (xhr) { contacts = JSON.parse(xhr.responseText); });
+var getregions = load('/data/poland-admin.json', function (xhr) { regions = JSON.parse(xhr.responseText); });
+var getcountries = load('/data/world-countries.json', function (xhr) { countries = JSON.parse(xhr.responseText); });
+
+$.when(getcolors, getcontacts, getregions, getcountries)
+    .done(function() {
+
+        function addLayer(collection) {
+            $.each(collection.features,
+                function(i, v) {
+                    var name = v.properties.name;
+                    v.properties.style = {};
+                    v.properties.style.fillColor = colors[name];
+                    v.properties.style.fillOpacity = contacts[name] ? 0.4 : 0.2;
+                });
+            var geojson = L.geoJson(collection,
+                {
+                    onEachFeature: onEachFeature,
+                    style: function(feature) { return feature.properties && feature.properties.style; }
+                })
+                .addTo(map);
+            return geojson;
+        }
+
+        function onEachFeature(feature, layer) {
+            var properties = feature.properties;
+            var id = feature.id;
+            var c = contacts[id];
+
+            function clickFeature(e) {
+                var layer = e.target;
+                //map.fitBounds(layer.getBounds());
+                if (c) {
+                    var people = c.people;
+                    var name, list = '';
+                    for (name in people) {
+                        if (people.hasOwnProperty(name)) {
+                            var contact = c[name];
+                            list += '<h4>' + name + '</h4>' + '<p>' + contact.comment + '</p>';
+                            if (contact.email) list += '<p>E-Mail: ' + contact.email + '</p>';
+                            if (contact.phone) list += '<p>Phone: ' + contact.phone + '</p>';
+                        }
+                    }
+                }
+                $('#contactDetails').html(list);
+                //console.log(layer.feature.properties.name); //country info from geojson
+            }
+
+            properties.popup = '<b class=popupheader>' + name + '</b>';
+
+            if (c) {
+                properties.popup = '<p><b class=popupheader>' + c.orgname + '</b>('+c.website+')</p>';
+                $.each(c.people,
+                    function(i, v) {
+                        properties.popup += '<p><i>' + i + '</i></p>';
+                    });
+            }
+            layer.on('mouseover',
+                function(e) {
+                    $('#mapTopRight').html(properties.popup);
+                })
+            .on('mouseout',
+                function(e) {
+                    $('#mapTopRight').html('');
+
+                })
+            .on({
+                click: clickFeature
+            });
+        }
+
+        var countrylayer = addLayer(countries);
+        var regionlayer = addLayer(regions);
+
+        map.on({
+            click: function() {
+                map.setView([0, 0], 2);
+            }
+        });
+
+    });
